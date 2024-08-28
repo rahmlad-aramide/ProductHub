eb150b221d8d7f97b70e83764451c4303e5f8723
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2024-08-28 09:51:53 +0100

    update: updated the deployment url

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://producthub.vercel.app/sitemap.xml",
+    sitemap: "https://producthub-rahmlad.vercel.app/sitemap.xml",
   };
 }
